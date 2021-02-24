68952c38b20281682c64508b1b566cf1d76bc669
Labanya Mukhopadhyay <labanyam@hotmail.com>
2021-02-24 11:20:18 -0800

    change db fields

--- reports/models.py
+++ reports/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Report(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    sex = models.CharField(max_length=20)
+    dob = models.CharField(max_length=4)
+    id_notes = models.TextField()
+    data_owners = models.CharField(max_length=20)
+    status = models.CharField(max_length=20)
+    bio = models.TextField()
     image = models.FilePathField(path="/img")
