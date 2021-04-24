67d3efaa6321ba0a33816af8255de7f15b04edb2
Labanya Mukhopadhyay <labanyam@hotmail.com>
2021-04-23 17:57:22 -0700

    delete proj

--- reports/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.report_index, name="report_index"),
-    path("<int:pk>/", views.report_detail, name="report_detail"),
-]
-
